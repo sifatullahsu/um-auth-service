238f471a005748fc74166764108f55f0686e7eb5
sifatullahsu <personal.sifat@gmail.com>
2023-06-12 12:34:33 +0600

    conditional logging for development and production

--- src/app/shared/logger.ts
+++ src/app/shared/logger.ts
@@ -3,36 +3,40 @@ import { createLogger, format, transports } from 'winston'
 import DailyRotateFile from 'winston-daily-rotate-file'
 const { combine, timestamp, printf } = format
 
-const myFormat = printf(({ level, message, timestamp }) => {
+const customFormat = printf(({ level, message, timestamp }) => {
   return `${timestamp} [PH] ${level}: ${message}`
 })
 
+const customTransports = (name: string) => {
+  const basic = [new transports.Console()]
+
+  return process.env.NODE_ENV === 'production'
+    ? [
+        ...basic,
+        new DailyRotateFile({
+          filename: path.join(
+            process.cwd(),
+            'logs',
+            name,
+            `${name}-%DATE%.log`
+          ),
+          datePattern: 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d'
+        })
+      ]
+    : basic
+}
+
 export const logger = createLogger({
   level: 'info',
-  format: combine(timestamp(), myFormat),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(process.cwd(), 'logs', 'info', 'info-%DATE%.log'),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
+  format: combine(timestamp(), customFormat),
+  transports: customTransports('info')
 })
 
 export const errorLogger = createLogger({
   level: 'error',
-  format: combine(timestamp(), myFormat),
-  transports: [
-    new transports.Console(),
-    new DailyRotateFile({
-      filename: path.join(process.cwd(), 'logs', 'error', 'error-%DATE%.log'),
-      datePattern: 'YYYY-MM-DD-HH',
-      zippedArchive: true,
-      maxSize: '20m',
-      maxFiles: '14d',
-    }),
-  ],
+  format: combine(timestamp(), customFormat),
+  transports: customTransports('error')
 })
