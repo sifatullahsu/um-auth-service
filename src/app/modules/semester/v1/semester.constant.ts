95bf3f88c5464b6952ba3b0b589af02a18d7bb8f
sifatullahsu <personal.sifat@gmail.com>
2023-06-14 10:53:49 +0600

    semsester pre save checking

--- src/app/modules/semester/v1/semester.constant.ts
+++ src/app/modules/semester/v1/semester.constant.ts
@@ -18,3 +18,9 @@ export const semesterMonth: iSemesterMonth[] = [
   'November',
   'December'
 ]
+
+export const semesterTitleCodeMapper: { [key: string]: string } = {
+  Autumn: '01',
+  Summer: '02',
+  Fall: '03'
+}
--- src/app/modules/semester/v1/semester.model.ts
+++ src/app/modules/semester/v1/semester.model.ts
@@ -1,32 +1,59 @@
+import httpStatus from 'http-status'
 import { model, Schema } from 'mongoose'
-import { semesterCode, semesterMonth, semesterTitle } from './semester.constant'
+import ApiError from '../../../../errors/ApiError'
+import {
+  semesterCode,
+  semesterMonth,
+  semesterTitle,
+  semesterTitleCodeMapper
+} from './semester.constant'
 import { iSemester, iSemesterModel } from './semester.interface'
 
-const semesterSchema = new Schema<iSemester, iSemesterModel>({
-  title: {
-    type: String,
-    required: true,
-    enum: semesterTitle
+const semesterSchema = new Schema<iSemester, iSemesterModel>(
+  {
+    title: {
+      type: String,
+      required: true,
+      enum: semesterTitle
+    },
+    year: {
+      type: Number,
+      required: true
+    },
+    code: {
+      type: String,
+      required: true,
+      enum: semesterCode
+    },
+    startMonth: {
+      type: String,
+      required: true,
+      enum: semesterMonth
+    },
+    endMonth: {
+      type: String,
+      required: true,
+      enum: semesterMonth
+    }
   },
-  year: {
-    type: Number,
-    required: true
-  },
-  code: {
-    type: String,
-    required: true,
-    enum: semesterCode
-  },
-  startMonth: {
-    type: String,
-    required: true,
-    enum: semesterMonth
-  },
-  endMonth: {
-    type: String,
-    required: true,
-    enum: semesterMonth
+  {
+    timestamps: true
   }
+)
+
+semesterSchema.pre('save', async function (next) {
+  const query = { $and: [{ title: this.title }, { year: this.year }] }
+  const isExist = await Semester.findOne(query)
+
+  if (isExist) {
+    throw new ApiError(httpStatus.CONFLICT, 'Semester is Already Exist!')
+  }
+
+  if (semesterTitleCodeMapper[this.title] !== this.code) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code')
+  }
+
+  next()
 })
 
 export const Semester = model<iSemester, iSemesterModel>('Semester', semesterSchema)
